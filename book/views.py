8b2a5ac48c8dd0d9af5685ebc84a6c0da7c40b27
admin <admin@example.com>
2018-11-23 07:39:15 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- book/views.py
+++ book/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
 
 
 def book_list(request):
     books = Book.objects.all().order_by('published_date')
     return render(request, 'book/book_list.html', {'books': books})
+
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'book/book_detail.html', {'book': book})
 

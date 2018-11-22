e9718ac323d2036bba99b9f70b0e7cb34d80d6f2
admin <admin@example.com>
2018-11-23 08:42:08 +0900

    Added appname to urls and each template

--- book/urls.py
+++ book/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 
+app_name = 'book'
 urlpatterns = [
     path('', views.book_list, name='book_list'),
     path('book/<int:pk>/', views.book_detail, name='book_detail'),
